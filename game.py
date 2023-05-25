69f72bd287de678ec8d5ef623a4b7e459dacbb75
Arliqn <131873538+Arliqn@users.noreply.github.com>
2023-05-25 19:43:01 +0600

    game.py

--- game.py
+++ game.py
@@ -1,24 +1,22 @@
 import pygame
 import math
 
-# Initialize pygame
-pygame.init()
+pygame.init()         # Initializing pygame
 
 # Set up the display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Circle Drawing Game")
 
-# Colors
+# Define colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Circle parameters
+# Adding circle parameters
 circle_radius = 100
 circle_center = (width // 2, height // 2)
 
-# Mouse position
-mouse_position = (0, 0)
+mouse_position = (0, 0)         # Mouse position
 
 # Game loop
 running = True
@@ -27,10 +25,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEMOTION:
-            # Update the mouse position
-            mouse_position = pygame.mouse.get_pos()
+            
+            
+            mouse_position = pygame.mouse.get_pos()             # Updating the mouse position
 
-    # Clear the screen
+    # Очистка
     screen.fill(BLACK)
 
     # Draw the perfect circle
@@ -39,22 +38,22 @@ while running:
     # Draw the user's circle
     pygame.draw.circle(screen, WHITE, mouse_position, circle_radius, 1)
 
-    # Calculate the mean square error (MSE) between the two circles
+    # Calculate the mean sqrt errror
     dx = circle_center[0] - mouse_position[0]
     dy = circle_center[1] - mouse_position[1]
     distance = math.sqrt(dx ** 2 + dy ** 2)
     mse = (distance - circle_radius) ** 2
 
-    # Calculate the perfection score
+    # Calculate the score
     perfection = max(0, 1 - (mse / (circle_radius ** 2)))
 
-    # Display the perfection score
+    # Display the score
     font = pygame.font.Font(None, 36)
     text = font.render(f"Perfection: {perfection:.2%}", True, WHITE)
     screen.blit(text, (10, 10))
 
-    # Update the display
+    # Update
     pygame.display.flip()
 
-# Quit the game
+# Quit
 pygame.quit()
